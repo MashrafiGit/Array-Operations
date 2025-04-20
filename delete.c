31ca2114c73608fa8f9c09a32730a9cff39dfac4
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-20 15:37:40 +0600

    delete value from and array

---
+++ delete.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int inx;
+    scanf("%d", &inx);
+
+    for (int i = inx; i < n - 1; i++)
+    {
+        a[i] = a[i + 1];
+    }
+
+    n--;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
